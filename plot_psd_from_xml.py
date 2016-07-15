23f2ee6825a07d5b9402061b5236424a388d2b13
Eric Chassande-Mottin <eric.chassandemottin@ligo.org>
2016-07-15 22:48:25 +0200

    compare with PSD model from Prospects paper

--- plot_psd_from_xml.py
+++ plot_psd_from_xml.py
@@ -2,6 +2,7 @@ from __future__ import division
 
 import sys
 import math
+import numpy
 
 import matplotlib
 import matplotlib.pyplot as plot
@@ -28,8 +29,26 @@ matplotlib.rc('font', **font)
 class LIGOLWContentHandler(ligolw.LIGOLWContentHandler):
         pass
 
-#filename = "psds_2016-17.xml"
-filename = "psd.xml"
+# from the Prospect paper
+#
+# data = numpy.array([
+#     [10, 1.0e-43,  numpy.nan],
+#     [20, 8.1e-45,  numpy.nan],
+#     [30, 1.6e-45,   3.6e-43],
+#     [40, numpy.nan,   4e-44],
+#     [60,  1.2e-46,  2.5e-45],
+#     [100, 4.9e-47,  4.4e-46],
+#     [200, 4.9e-47,  4.4e-46],
+#     [500, 8.1e-47,  1.0e-45],
+#     [1000, numpy.nan, 3.6e-45],
+#     [1200, 4e-46,  numpy.nan],
+#     [2000, 9e-46,   1.4e-44]])
+
+data_adv = numpy.loadtxt("Adv_Virgo_20Mpc_PSD.txt",dtype={'names':('f','psd'), 'formats':('f8','f8')})
+data_aligo = numpy.loadtxt("aLIGO_80Mpc_PSD.txt",dtype={'names':('f','psd'), 'formats':('f8','f8')})
+
+filename = "psds_2016-17.xml"
+#filename = "psd.xml"
 duration = 32 # sec
 sample_rate = 1024 # Hz
 
@@ -44,6 +63,12 @@ for detector, model in psd_models.iteritems():
     psd = lal.CreateREAL8FrequencySeries(None, lal.LIGOTimeGPS(0), 0, 1./duration, filter.unitInverseHertz, int((duration * sample_rate) / 2 + 1))
     psd.data.data = model(filter.abscissa(psd))
     plot.loglog(filter.abscissa(psd), psd.data.data, label=detector)
+
+# plot.loglog(data[:,0],data[:,1])
+# plot.loglog(data[:,0],data[:,2])
+
+plot.loglog(data_adv['f'],data_adv['psd'], label="V1 from Prospects paper")
+plot.loglog(data_aligo['f'],data_aligo['psd'], label="H1 and L1 from Prospects paper")
 
 plot.legend(loc=1)
 plot.grid(True)
