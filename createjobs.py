00741c64921ac373759ed80b1be8d5bc487d03ff
Eric Chassande-Mottin <eric.chassande-mottin@ligo.org>
2016-09-22 22:35:17 +0200

    few fixes -- beta version

--- createjobs.py
+++ createjobs.py
@@ -10,12 +10,13 @@ from glue.ligolw import ilwd
 from glue.ligolw import lsctables
 from glue.ligolw import utils as ligolw_utils
 
-LITTLEHOPE_OPTS = '--detector H1 --detector L1 --min-triggers 2 --snr-threshold 4.0 --reference-psd psds_2016-17.xml --template-bank templates.xml --waveform "TaylorF2threePointFivePN"'
-CMD_LITTLEHOPE = './my_bayestar_littlehope  {} {{}} -o coinc.xml'.format(LITTLEHOPE_OPTS)
+LITTLEHOPE_HOME = os.getcwd()
+LITTLEHOPE_OPTS = '--detector H1 --detector L1 --min-triggers 2 --snr-threshold 4.0 --reference-psd {home}/psds_2016-17.xml --template-bank {home}/templates.xml --waveform "TaylorF2threePointFivePN"'.format(home=LITTLEHOPE_HOME)
+CMD_LITTLEHOPE = '{home}/my_bayestar_littlehope  {opts} {mdc_file} -o {simdir}/coinc.xml\n'
 
 LOCALCOINCS_OPTS = '--waveform "TaylorF2threePointFivePN" --f-low 30'
-CMD_LOCALCOINCS = 'bayestar_localize_coincs {} coinc.xml'.format(LOCALCOINCS_OPTS)
-CMD_JOBSUB = '/opt/sge/bin/lx24-amd64/qsub -N {} -o {} {} {}'
+CMD_LOCALCOINCS = 'bayestar_localize_coincs {opts} {simdir}/coinc.xml\n'
+CMD_JOBSUB = '/opt/sge/bin/lx24-amd64/qsub -N {} -o {} -e {} {}'
 
 if __name__ == "__main__":
 
@@ -41,21 +42,33 @@ if __name__ == "__main__":
 
         copy(indir + '/' + file, simdir + '/')
         
+        fullpath_simdir = os.path.abspath(simdir)
+
         # create job scripts
         batch_filename = "{}/batch.sh".format(simdir)
         with open(batch_filename, "w") as batch_script:
-            batch_script.write("#!/usr/bin/env bash")
-            batch_script.write("cd {}".format(simdir))
-            batch_script.write(CMD_LITTLEHOPE.format(file))
-            batch_script.write(CMD_LOCALCOINCS.format(file))
+            batch_script.write("#!/usr/bin/env bash\n")
+            batch_script.write("cd {}\n".format(fullpath_simdir))
+            batch_script.write(CMD_LITTLEHOPE.format(mdc_file=file,
+                                                     simdir=fullpath_simdir,
+                                                     home=LITTLEHOPE_HOME,
+                                                     opts=LITTLEHOPE_OPTS))
+            batch_script.write(CMD_LOCALCOINCS.format(simdir=fullpath_simdir,
+                                                      opts=LOCALCOINCS_OPTS))
+            batch_script.write('echo "hello word!"')
             #batch_script.write(CMD_PLOT.format(file))
 
         os.chmod(batch_filename, 0744)
         
         # create script with submission list
-        submission_cmds.append(CMD_JOBSUB.format(simdir, "{}/{}.txt".format(simdir,simdir), "", batch_filename))
+        submission_cmds.append(CMD_JOBSUB.format(simdir, 
+                                                 "{}/{}.out".format(fullpath_simdir,simdir), 
+                                                 "{}/{}.err".format(fullpath_simdir,simdir), 
+                                                 batch_filename))
 
     outfile = open('jobsubmission.sh', 'w')
     print>>outfile, "#!/usr/bin/env bash"
     for line in submission_cmds:
         print>>outfile, line
+
+    os.chmod('jobsubmission.sh', 0744)
