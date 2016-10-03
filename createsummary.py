5c32764ad4c642453304886b2676bc04b3722ca3
Eric Chassande-Mottin <eric.chassande-mottin@ligo.org>
2016-10-03 18:31:51 +0200

    include skymap matching

--- createsummary.py
+++ createsummary.py
@@ -2,6 +2,8 @@
 from __future__ import division
 
 import sys
+import os
+
 import lal
 from lal import gpstime
 
@@ -11,33 +13,48 @@ from glue.ligolw import lsctables
 
 from lalinference.bayestar import ligolw as ligolw_bayestar
 
-xmldoc1 = ligolw_utils.load_filename(sys.argv[1],
+file1 = sys.argv[1]
+(path1, filename1) = os.path.split(file1)
+
+file2 = sys.argv[2]
+(path2, filename2) = os.path.split(file2)
+
+xmldoc1 = ligolw_utils.load_filename(file1,
                                         contenthandler=ligolw_bayestar.LSCTablesContentHandler)
+
+xmldoc2 = ligolw_utils.load_filename(file2,
+                                        contenthandler=ligolw_bayestar.LSCTablesContentHandler)
+
 sim_inspiral_table = ligolw_table.get_table(xmldoc1,
                                         lsctables.SimInspiralTable.tableName)
 
-xmldoc2 = ligolw_utils.load_filename(sys.argv[2],
-                                         contenthandler=ligolw_bayestar.LSCTablesContentHandler)
+coinc_inspiral_table = ligolw_table.get_table(xmldoc2, 
+                                              lsctables.CoincInspiralTable.tableName)
 
-print "# GPS mass1 mass2 dist SNR RA dec"
-    
-for coinc, sngl_inspirals in ligolw_bayestar.coinc_and_sngl_inspirals_for_xmldoc(xmldoc2):
+print "# GPS mass1 mass2 dist SNR RA dec inclination skymap"
 
-    sngl_inspiral = sngl_inspirals[0]
+for coinc in coinc_inspiral_table:
 
-    for sim_inspiral in sim_inspiral_table:
-        if sim_inspiral.geocent_end_time == sngl_inspiral.end_time:
-            break
+        for sim_inspiral in sim_inspiral_table:
+            if abs(sim_inspiral.geocent_end_time-coinc.end_time) < 2:
+                break
+            else:
+                sim_inspiral = None
 
-    end_time = lal.LIGOTimeGPS(sim_inspiral.geocent_end_time, sim_inspiral.geocent_end_time_ns)
-    (RA,dec) = sim_inspiral.ra_dec
+        if not sim_inspiral:
+            continue
 
-    print '{gps};"{date}";{mass1};{mass2};{dist};{snr};{RA};{dec}'.format(gps=end_time,
-                                                                        date=lal.gpstime.gps_to_utc(end_time).isoformat(' '),
-                                                                        mass1=sim_inspiral.mass1,
-                                                                        mass2=sim_inspiral.mass2,
-                                                                        dist=sim_inspiral.distance,
-                                                                        #dist=sngl_inspiral.eff_distance,
-                                                                        snr=sngl_inspiral.snr,
-                                                                        RA=RA,
-                                                                        dec=dec)
+        end_time = lal.LIGOTimeGPS(sim_inspiral.geocent_end_time, sim_inspiral.geocent_end_time_ns)
+        (RA,dec) = sim_inspiral.ra_dec
+        event_id = str(coinc.coinc_event_id)
+
+        print '{gps};"{date}";{mass1};{mass2};{dist};{snr};{RA};{dec};{inclination};{skymap}'.format(gps=end_time,
+                                                                                                     date=lal.gpstime.gps_to_utc(end_time).isoformat(' '),
+                                                                                                     mass1=sim_inspiral.mass1,
+                                                                                                     mass2=sim_inspiral.mass2,
+                                                                                                     dist=sim_inspiral.distance,
+                                                                                                     snr=coinc.snr,
+                                                                                                     RA=RA,
+                                                                                                     dec=dec,
+                                                                                                     inclination=sim_inspiral.inclination,                     
+                                                                                                     skymap="{}/{}.toa_phoa_snr.fits.gz".format(path1,event_id[-1]))
