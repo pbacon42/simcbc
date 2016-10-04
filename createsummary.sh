046966e21195148f8d8016eb0012e1fb633c1ac3
Eric Chassande-Mottin <eric.chassande-mottin@ligo.org>
2016-10-04 08:44:48 +0200

    restrict to current folder

--- createsummary.sh
+++ createsummary.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-for d in `find . -type d -name 'mdc*[0-9]'`; do
+for d in `find . -type d -name 'mdc*[0-9]' -maxdepth 1`; do
     python -W ignore createsummary.py $d/$d.xml $d/coinc.xml 
 done
